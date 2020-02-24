cf049ff460ccf11452b4bee6f3eb63a531b5fa35
Daryl McAvoy <dmcavoy631@gmail.com>
2020-02-24 08:53:10 -0600

    1st round

--- main.js
+++ main.js
@@ -50,33 +50,186 @@ const arrOfPeople = [
   },
 ]
 
+
+
+class player {
+  constructor(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+  this.person = [person]
+  this.canThrowBall = canThrowBall,
+  this.canDodgeBall = canDodgeBall,
+  this.hasPaid = hasPaid,
+  this.isHealthy = isHealthy,
+  this.yearsExperience = yearsExperience,
+  this.players = [];
+  }
+  addPlayer(player){
+    this.players.push(player);
+  }
+}
+
+
+
+class blueTeammate extends player {
+  constructor(mascot, color){
+    super()
+    this.mascot = mascot;
+    this.color = color;
+    this.blueTeam = []
+  }
+  addMember(bluePlayer){
+    this.blueTeam.push(bluePlayer);
+  }
+}
+
+
+
+class redTeammate extends player {
+  constructor(mascot, color){
+    super()
+    this.mascot = mascot;
+    this.color = color;
+    this.redTeam = []
+  }
+  addMember(redPlayer){
+    this.redTeam.push(redPlayer);
+  }
+}
+
+let newblueTeammate = new blueTeammate("Bananan Slugs", "Blue");
+let newredTeammate = new redTeammate("Cobras", "Red");
 const listOfPlayers = []
 const blueTeam = []
 const redTeam = []
 
-class player {
-  constructor(){}
+
+// create new class
+function addBlueTeam(bluePlayer){
+  for(let ele of listOfPlayers){
+    if (ele == bluePlayer){
+      listOfPlayers.splice(ele,1);
+    }
+  }
+  blueTeam.push(bluePlayer)
+  newblueTeammate.addMember(bluePlayer)
+  updateDisplay()
 }
-class blueTeammate {
-  constructor(){}
+
+
+// create new class
+function addRedTeam(redPlayer){
+  for(let ele of listOfPlayers){
+    if (ele == redPlayer){
+      listOfPlayers.splice(ele,1);
+    }
+  }
+  redTeam.push(redPlayer)
+  newredTeammate.addMember(redPlayer)
+  updateDisplay()
 }
-class redTeammate {
-  constructor(){}
+
+function updateDisplay(id) {
+ 
+  const listElementR = document.getElementById('Red')
+  listElementR.innerHTML = ""
+
+  redTeam.map(person => {
+    const li = document.createElement("li")
+    li.setAttribute("id", person.id)
+    li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+    listElementR.append(li)
+  })
+
+  const listElementB = document.getElementById('Blue')
+  listElementB.innerHTML = ""
+
+  blueTeam.map(person =>{
+    const li = document.createElement("li")
+    li.setAttribute("id", person.id)
+    console.log(person.id)
+    li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+    listElementB.append(li)
+  })
+
+   const listElementP = document.getElementById('players')
+  listElementP.innerHTML = ""
+
+  listOfPlayers.map(person =>{
+    const li = document.createElement("li")
+    li.setAttribute("id", person.id)
+
+    // Creating button for Blue Team
+    const buttonB = document.createElement("button")
+    buttonB.setAttribute("id","blueTeam")
+    buttonB.innerHTML = "Make Blue Player"
+    buttonB.addEventListener('click', function() {addBlueTeam(person);})
+    
+    // Creating button for Red Team
+    const buttonR = document.createElement("button")
+    buttonR.setAttribute("id","redTeam")
+    buttonR.innerHTML = "Make Red Player"
+    buttonR.addEventListener('click', function() {addRedTeam(person);})
+
+    // Addd Buttons to List Items
+    li.appendChild(buttonB)
+    li.appendChild(buttonR)
+    li.appendChild(document.createTextNode(person.name))
+    listElementP.append(li)
+  })
 }
+
+
+
 
 const listPeopleChoices = () => {
   const listElement = document.getElementById('people')
+  listElement.innerHTML = ""
+
   arrOfPeople.map(person => {
+
     const li = document.createElement("li")
+    li.setAttribute("id", person.id)
+
     const button = document.createElement("button")
     button.innerHTML = "Make Player"
     button.addEventListener('click', function() {makePlayer(person.id)} )
+
     li.appendChild(button)
     li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+
     listElement.append(li)
   })
 }
 
+
 const makePlayer = (id) => {
-  console.log(`li ${id} was clicked!`)
+  // console.log(`li ${id} was clicked!`)
+  document.getElementById(id).remove();
+  for(let obj of arrOfPeople){
+    if (obj.id == id){
+
+      // Add to Players , Remove from People
+      listOfPlayers.push(obj);
+      arrOfPeople.splice(obj,1)
+
+      // Update all list with .map
+      updateDisplay(id);
+    }
+  }
+}
+
+
+// Testing 
+
+if (typeof describe === "function") {
+  describe("addRedTeam(redPlayer)", () => {
+  it("add player to red team", () => {
+    addRedTeam("test red");
+});
+  });
+  describe("addBlueTeam(bluePlayer)", () => {
+    it("add player to blue team", () => {
+      addRedTeam("test blue");
+  });
+});
+
 }
